cb8d99e787e7f0912627b5ca27992defe8c68bb6
limy <1511268879@qq.com>
2021-01-05 11:19:15 +0800

    Update build.js

--- wj-vue/src/components/home/Appindex.vue
+++ wj-vue/src/components/home/Appindex.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
-    Hello World!
-  </div>
+    <div>
+      Hello World!
+    </div>
 </template>
 
 <script>
--- wj/src/test/java/com/SEdesign/notebook/NotebookApplicationTests.java
+++ wj/src/test/java/com/SEdesign/notebook/NotebookApplicationTests.java
@@ -9,5 +9,4 @@ class NotebookApplicationTests {
     @Test
     void contextLoads() {
     }
-
 }
